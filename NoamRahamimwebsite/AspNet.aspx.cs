79c34a722c621dbce76e9424bd5e1f898987205a
noam-mor-2025-katza <noam3mor@gmail.com>
2025-12-02 16:48:03 +0200

    לוח הכפל

---
+++ NoamRahamimwebsite/AspNet.aspx.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class AspNet : System.Web.UI.Page
+{
+    public string str;
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+        str = "<table border='1'>";
+        for (int i = 1; i <= 10; i++)
+        {
+            str += "<tr/style='backround-color:blue'/>"; //notworkingyet
+            for (int j = 1; j <= 10; j++)
+            {
+                str += "<td>";
+                str += i * j;
+                str += "</td>";
+            }
+            str += "</tr>";
+        }
+        str += "</table>";
+    }
+}
